d0ece507b704613a1b1471cef426a1c3d3007c7a
AkilTDixon <113859238+AkilTDixon@users.noreply.github.com>
2025-09-18 16:43:08 -0400

    added outputpath to wide string

--- ScreenRecorder/main.cpp
+++ ScreenRecorder/main.cpp
@@ -219,7 +219,8 @@ void capture(int startX, int startY, int endX, int endY, Window* timeWindow)
     
     double ms = (1.0 / (double)FPS) * 1000;
     string videoName = outputPath+"vid.mp4";
-    wstring wVideoName = L"vid.mp4";
+    wstring wVideoName = (LPCWSTR)outputPath.c_str();
+    wVideoName += L"vid.mp4";
     VideoWriter writer(videoName,
         VideoWriter::fourcc('H', '2', '6', '4'),
         FPS, Size(screenX, screenY));
